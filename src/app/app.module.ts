fd93574fb732fa0b701526036bbf1f5be0fcf49a
multimediav22 <42384264+multimediav22@users.noreply.github.com>
2023-11-27 02:01:05 -0500

    Actualización href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
@@ -9,19 +9,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     GifsModule,
     SharedModule,
     FormsModule,
-    // Corrige el nombre del módulo aquí
     HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,20 @@
+// Importa los módulos necesarios desde Angular
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-card', // Debería ser singular para representar una sola tarjeta
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardComponent implements OnInit {
+  @Input() gif!: Gif;
+
+  ngOnInit(): void {
+    if (!this.gif) {
+      throw new Error('Gif is required');
+    }
+  }
+}
